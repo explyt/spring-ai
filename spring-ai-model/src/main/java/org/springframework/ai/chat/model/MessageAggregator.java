def6bf4e8c7061cd601c370ef5dcdf98f8fabde1
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2025-06-05 17:06:26 +0300

    feat: Deploy to Explyt

--- spring-ai-model/src/main/java/org/springframework/ai/chat/model/MessageAggregator.java
+++ spring-ai-model/src/main/java/org/springframework/ai/chat/model/MessageAggregator.java
@@ -22,8 +22,6 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 
 import org.springframework.ai.chat.messages.AssistantMessage;
@@ -45,8 +43,6 @@ import org.springframework.util.StringUtils;
  * @since 1.0.0
  */
 public class MessageAggregator {
-
-	private static final Logger logger = LoggerFactory.getLogger(MessageAggregator.class);
 
 	public Flux<ChatResponse> aggregate(Flux<ChatResponse> fluxChatResponse,
 			Consumer<ChatResponse> onAggregationComplete) {
@@ -147,7 +143,7 @@ public class MessageAggregator {
 			metadataPromptMetadataRef.set(PromptMetadata.empty());
 			metadataRateLimitRef.set(new EmptyRateLimit());
 
-		}).doOnError(e -> logger.error("Aggregation Error", e));
+		});
 	}
 
 	public record DefaultUsage(Integer promptTokens, Integer completionTokens, Integer totalTokens) implements Usage {
