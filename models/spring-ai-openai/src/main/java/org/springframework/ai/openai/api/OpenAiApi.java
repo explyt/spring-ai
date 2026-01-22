e2247deeb9bf263112fa8eeec2c65dcd26da3c09
Ilya Muromtsev <let4time@gmail.com>
2026-01-23 01:31:45 +0300

    feat: GLM 4.7 related support

--- models/spring-ai-openai/src/main/java/org/springframework/ai/openai/api/OpenAiApi.java
+++ models/spring-ai-openai/src/main/java/org/springframework/ai/openai/api/OpenAiApi.java
@@ -20,7 +20,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -1146,7 +1145,8 @@ public class OpenAiApi {
 			@JsonProperty("reasoning_effort") String reasoningEffort,
 			@JsonProperty("web_search_options") WebSearchOptions webSearchOptions,
 			@JsonProperty("verbosity") String verbosity,
-			@JsonProperty("prompt_cache_key") String promptCacheKey)  {
+			@JsonProperty("prompt_cache_key") String promptCacheKey,
+			@JsonProperty("thinking") Thinking thinking) {
 
 		/**
 		 * Shortcut constructor for a chat completion request with the given messages, model and temperature.
@@ -1158,7 +1158,7 @@ public class OpenAiApi {
 		public ChatCompletionRequest(List<ChatCompletionMessage> messages, String model, Double temperature) {
 			this(messages, model, null, null, null, null, null, null, null, null, null, null, null, null, null,
 					null, null, null, false, null, temperature, null,
-					null, null, null, null, null, null, null, null);
+					null, null, null, null, null, null, null, null, null);
 		}
 
 		/**
@@ -1172,7 +1172,7 @@ public class OpenAiApi {
 			this(messages, model, null, null, null, null, null, null,
 					null, null, null, List.of(OutputModality.AUDIO, OutputModality.TEXT), audio, null, null,
 					null, null, null, stream, null, null, null,
-					null, null, null, null, null, null, null, null);
+					null, null, null, null, null, null, null, null, null);
 		}
 
 		/**
@@ -1187,7 +1187,7 @@ public class OpenAiApi {
 		public ChatCompletionRequest(List<ChatCompletionMessage> messages, String model, Double temperature, boolean stream) {
 			this(messages, model, null, null, null, null, null, null, null, null, null,
 					null, null, null, null, null, null, null, stream, null, temperature, null,
-					null, null, null, null, null, null, null, null);
+					null, null, null, null, null, null, null, null, null);
 		}
 
 		/**
@@ -1203,7 +1203,7 @@ public class OpenAiApi {
 				List<FunctionTool> tools, Object toolChoice) {
 			this(messages, model, null, null, null, null, null, null, null, null, null,
 					null, null, null, null, null, null, null, false, null, 0.8, null,
-					tools, toolChoice, null, null, null, null, null, null);
+					tools, toolChoice, null, null, null, null, null, null, null);
 		}
 
 		/**
@@ -1216,7 +1216,7 @@ public class OpenAiApi {
 		public ChatCompletionRequest(List<ChatCompletionMessage> messages, Boolean stream) {
 			this(messages, null, null, null, null, null, null, null, null, null, null,
 					null, null, null, null, null, null, null, stream, null, null, null,
-					null, null, null, null, null, null, null, null);
+					null, null, null, null, null, null, null, null, null);
 		}
 
 		/**
@@ -1229,7 +1229,21 @@ public class OpenAiApi {
 			return new ChatCompletionRequest(this.messages, this.model, this.store, this.metadata, this.frequencyPenalty, this.logitBias, this.logprobs,
 			this.topLogprobs, this.maxTokens, this.maxCompletionTokens, this.n, this.outputModalities, this.audioParameters, this.presencePenalty,
 			this.responseFormat, this.seed, this.serviceTier, this.stop, this.stream, streamOptions, this.temperature, this.topP,
-			this.tools, this.toolChoice, this.parallelToolCalls, this.user, this.reasoningEffort, this.webSearchOptions, this.verbosity, null);
+			this.tools, this.toolChoice, this.parallelToolCalls, this.user, this.reasoningEffort, this.webSearchOptions, this.verbosity, this.promptCacheKey, this.thinking);
+		}
+
+		/**
+		 * This param is introduced to support GLM 4.7.
+		 *
+		 * @param type - enabled or disabled
+		 * @param clearThinking - should be false for the case Preserved thinking. Otherwise, it is Interleaved thinking
+		 *
+		 * @see <a href="https://docs.z.ai/guides/capabilities/thinking-mode">Z.ai GLM-4.7 Thinking Mode</a>
+		 */
+		@JsonInclude(Include.NON_NULL)
+		public record Thinking(
+				@JsonProperty("type") String type,
+				@JsonProperty("clear_thinking") Boolean clearThinking) {
 		}
 
 		/**
@@ -1442,7 +1456,7 @@ public class OpenAiApi {
 			@JsonProperty("refusal") String refusal,
 			@JsonProperty("audio") AudioOutput audioOutput,
 			@JsonProperty("annotations") List<Annotation> annotations,
-       		@JsonProperty(value = "reasoning_content", access = JsonProperty.Access.WRITE_ONLY) String reasoningContent,
+       		@JsonProperty("reasoning_content") String reasoningContent,
 			@JsonProperty(value = "reasoning", access = JsonProperty.Access.WRITE_ONLY) String reasoning
 	) { // @formatter:on
 
